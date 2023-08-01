13fbacfe842b0b9b52e6b32cd0d106b11fec54a8
OhSunJae <42952202+OhSunJae@users.noreply.github.com>
2023-08-02 01:16:13 +0900

    Add files via upload

---
+++ 001.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    puts("*첫 번째 프로그램*");
+    printf(" c프로그래밍은 어렵지만 재미있다. .\n");
+    printf("코딩을 끝냈으면 ctrl+f5키를 누르세요. \n");
+
+    return 0;
+}
---
+++ 004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    puts("문자열 출력 *");
+    puts(" puts은 문자열 출력 전용함수");
+
+    puts("입력함수 \t 출력함수");
+    puts("getchar \t putchar");
+    puts("gets    \t puts   ");
+    puts("scanf    \t printf");
+
+    puts(" 대칭관계를 잘 확인해보세요");
+
+    return 0;
+}
